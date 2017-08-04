90c6c872519f1f632111c265ecda274b4486f4d5
Peter Wildberger <pete.wildberger@gmail.com>
2017-08-04 15:38:01 -0500

    body-parser

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-// var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 
 //uses
 // app.use (express.static('public'));
