3d6b4662daec6ad8a9e7c321384fb1e27bc7387a
Peter Wildberger <pete.wildberger@gmail.com>
2017-08-04 15:41:13 -0500

    hosted

--- server.js
+++ server.js
@@ -19,7 +19,12 @@ app.listen(port, function(){
 app.get( '/', function(req, res){
   console.log('base url hit');
 
-  res.sendFile(path.resolve('/index.html'));
+  res.sendFile(path.resolve('index.html'));
+});
+app.get( '/dontcall', function(req, res){
+  console.log('base url hit');
+
+  res.sendFile(path.resolve('index.xml'));
 });
 app.get( '/dontcall', function(req, res){
   console.log('base url hit');
