4f8b35b5b6d81036c57ed9e9cafec8ec5e26e323
Peter Wildberger <pete.wildberger@gmail.com>
2017-08-04 15:31:51 -0500

    init

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+// var bodyParser = require('body-parser');
+
+//uses
+// app.use (express.static('public'));
+app.use( bodyParser.urlencoded( { extended: true } ) );
+
+//globals
+var port = process.env.PORT || 2017;
+
+//listen
+app.listen(port, function(){
+  console.log('up on 2017');
+});
+
+//base URL
+app.get( '/', function(req, res){
+  console.log('base url hit');
+
+  res.sendFile(path.resolve('/index.xml'));
+});
+app.get( '/dontcall', function(req, res){
+  console.log('base url hit');
+
+  res.sendFile(path.resolve('./dontcall.m4a'));
+});
